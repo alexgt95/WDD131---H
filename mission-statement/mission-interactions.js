24ffb93d5811a27b377ae77d636cd2a20ea78007
Alejandro Garcia Trujillo <ale.g.trujillo2005@gmail.com>
2026-01-17 22:36:44 -0600

    Image displaying on GitHub correction

--- mission-statement/mission-interactions.js
+++ mission-statement/mission-interactions.js
@@ -8,10 +8,10 @@ function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
         document.body.classList.add('dark');
-        logo.src = '/mission-statement/byui-logo-white.png';
+        logo.src = 'byui-logo-white.png';
     } else {
         document.body.classList.remove('dark');
-        logo.src = '/mission-statement/byui-logo-blue.webp';
+        logo.src = 'byui-logo-blue.webp';
     }
 }           
                     
