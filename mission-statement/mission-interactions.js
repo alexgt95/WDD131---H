5074e6b268c86d045664d9582b4e93a12cf2e71f
Alejandro Garcia Trujillo <ale.g.trujillo2005@gmail.com>
2026-01-17 22:32:26 -0600

    W02 Prove: Mission Statement II

---
+++ mission-statement/mission-interactions.js
@@ -0,0 +1,17 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.classList.add('dark');
+        logo.src = '/mission-statement/byui-logo-white.png';
+    } else {
+        document.body.classList.remove('dark');
+        logo.src = '/mission-statement/byui-logo-blue.webp';
+    }
+}           
+                    
